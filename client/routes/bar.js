f9106ed7d259a362aa1a40dbe4685fae76103acf
yejingfu <yejingfu@hotmail.com>
2015-01-30 23:19:24 +0800

    rest client

--- client/routes/bar.js
+++ client/routes/bar.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
 var multiparty = require('multiparty');
 var path = require('path');
@@ -43,38 +44,86 @@ router.post('/add', function(req, res) {
       console.log('get file name: ' + n);
     });
     */
-    console.log('bar: ' + fields['barName'] + '--' + fields['barAddress']);
+    console.log('bar: ' + fields['barName']+'--'+fields['barTel'] + '--' + fields['barAddress']+ '--' + fields['barDistrict']);
     console.log('latitude:' + fields['barLat'] + ', longitude:' + fields['barLong']);
     var images = files['barPhoto'];
-    console.log('bar photo len:'+ images.length);
-    if (!images || images.length === 0 || images[0].size === 0) {
+    if (!images || images.length === 0) {
       res.end('Failed to upload image: no image received');
       return;
     }
-    //printObject(img[0]);
-    var count = 0, len = images.length, ok = true;
-    for (var i = 0; i < len; i++) {
-      img = images[i];
-      var imgName = img.originalFilename;
-      var imgPath = img.path;
-      var imgNewName = path.basename(imgPath);
-      var imgNewPath = path.join(app.get('imagepath'), imgNewName);
-      console.log('moving image from: ' + imgPath + ' ===> to: ' + imgNewPath);
-      fs.move(imgPath, imgNewPath, function(err) {
+
+    var i, len, count, validImages = [], validImageNames = [];
+    for (i = 0, len = images.length; i < len; i++) {
+      if (images[i] && images[i].size !== 0) {
+        validImages.push(images[i]);
+      }
+    }
+    console.log('bar photo len:'+ validImages.length);
+    
+    var saveToDB = function() {
+      var barObj = {};
+      barObj.bname = fields['barName'];
+      barObj.tel = fields['barTel'];
+      barObj.address = fields['barAddress'];
+      barObj.latitude = fields['barLat'];
+      barObj.longitude = fields['barLong'];
+      barObj.district = fields['barDistrict'];
+      barObj.rank = 1;
+      barObj.photoes = validImageNames.join(';');
+      addBarToServer(barObj, function(err) {
+        if (!err)
+          console.log('succeed to addBarToServer');
+        else
+          console.log('failed to addBarToServer: ' + err);
+      });
+    };
+
+    var img, imgName, imgPath, imgNewName, imgNewPath;
+    for (i = 0, count = 0, len = validImages.length; i < len; i++) {
+      img = validImages[i];
+      imgName = img.originalFilename;
+      imgPath = img.path;
+      imgNewName = path.basename(imgPath);
+      imgNewPath = path.join(app.get('imagepath'), imgNewName);
+      console.log('==> image: ' + imgNewPath);
+      (function(from, to, newName){
+      fs.move(from, to, function(err) {
         count += 1;
-        if (ok) ok = !err;
+        if (!err) {
+          validImageNames.push(newName);
+        }
         if (count === len - 1) {
-          if (!ok) {
+          if (validImageNames.length === 0) {
             res.end('Failed to upload image');
           } else {
-            //res.end('upload done');
+            saveToDB();
             res.redirect('/bar');
           }
         }
       });
+      })(imgPath, imgNewPath, imgNewName);
     }
 
   });
 });
+
+var addBarToServer = function(barObj, cb) {
+  var options = {
+    hostname: '121.199.58.239',
+    port: 3011,
+    path: '/bar',
+    method: 'POST'
+  };
+  http.request(options, function(res) {
+    console.log('res status: ' + res.statusCode);
+    cb(0);
+  });
+  req.on('error', function(e) {
+    cb(e);
+  });
+  
+  req.write(JSON.stringify(barObj));
+  req.end();
+}; 
 
 module.exports = router;
